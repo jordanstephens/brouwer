18d925f3290414294221697966eb9f08ef77ddfd
Jordan Stephens <jordan@stephens.io>
2017-11-29 18:29:10 -0500

    switch to sgp4 and fix unit conversion error

--- src/TCanvasContainer.js
+++ src/TCanvasContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactAnimationFrame from 'react-animation-frame';
 import moment from 'moment';
-import satellite from 'satellite.js';
+import SGP4 from 'sgp4';
 
 import './TCanvasContainer.css';
 
@@ -10,6 +10,7 @@ const G = 6.674e-11;
 const EARTH_MASS = 5.972e+24;
 const MU = G * EARTH_MASS;
 const EARTH_RADIUS = 6371;
+const XP_DOT_P = 1440.0 / (2.0 * Math.PI); //229.1831180523293
 
 const PARAM_NAMES = [
   'semimajorAxis',
@@ -19,30 +20,73 @@ const PARAM_NAMES = [
   'argOfPerigee'
 ];
 
-function params2tle({ semimajorAxis, inclination = 0, rightAsc = 0, eccentricity = 0, argOfPerigee = 0, meanAnomaly = 0 }) {
-  inclination = Number(inclination).toFixed(4).padStart(8);
-  rightAsc = Number(rightAsc).toFixed(4).padStart(8);
-  eccentricity = eccentricity.toString().replace(/^0\./, '').padEnd(7, '0').slice(0, 7)
-  argOfPerigee = Number(argOfPerigee).toFixed(4).padStart(8);
-  meanAnomaly = Number(meanAnomaly).toFixed(4).padStart(8);
-  const meanMotion = Math.sqrt(MU / Math.pow(2 * semimajorAxis, 3)).toString().slice(0,11);
-  const satNo = '00001';
-  const satClass = 'U';
-  const satYear = '98';
-  const satMonth = '06';
-  return `
-    1 ${satNo}${satClass} ${satYear}${satMonth}1A   17330.48573378  .00003462  00000-0  59297-4 0  9990
-    2 ${satNo} ${inclination} ${rightAsc} ${eccentricity} ${argOfPerigee} ${meanAnomaly} ${meanMotion} 86996
-  `.split('\n').filter((x) => (x && x.length)).map(s=>s.trim()).join('\n');
+function newSatRec(params) {
+  var gravconst = SGP4.getgravconst("wgs84");
+
+  var now = new Date();
+
+  var opsmode = 'i';
+
+  var deg2rad  =  Math.PI / 180.0; //0.0174532925199433
+  var xpdotp   =  1440.0 / (2.0 * Math.PI); //229.1831180523293
+
+  var satrec = {};
+
+  satrec.error = 0;
+  satrec.whichconst = gravconst;
+
+  //Line 1
+  satrec.satnum = "12345"
+  satrec.epochyr = parseInt(now.getUTCFullYear().toString().substr(2, 2));
+  satrec.epochdays = (now - (new Date(now.getUTCFullYear(), 0, 0))) / (1000 * 60 * 60 * 24);
+  satrec.ndot = undefined; // ignoring for now
+  satrec.nddot = undefined; // ignoring for now
+  satrec.bstar = 0;
+
+  //Line 2
+  satrec.inclo = params.inclination || 0;
+  satrec.nodeo = params.rightAscension || 0;
+  satrec.ecco = params.eccentricity || 0;
+  satrec.argpo = params.argumentOfPerigee || 0;
+  satrec.mo = params.meanAnomaly || 0;
+  satrec.no = Math.sqrt(satrec.whichconst.mu / Math.pow(params.semimajorAxis, 3)) / (2 * Math.PI / 86400) / xpdotp;
+
+  satrec.inclo = satrec.inclo  * deg2rad;
+  satrec.nodeo = satrec.nodeo  * deg2rad;
+  satrec.argpo = satrec.argpo  * deg2rad;
+  satrec.mo    = satrec.mo     * deg2rad;
+
+  satrec.alta = satrec.a*(1.0 + satrec.ecco) - 1.0;
+  satrec.altp = satrec.a*(1.0 - satrec.ecco) - 1.0;
+
+  var year = 0;
+  if (satrec.epochyr < 57) {
+      year = satrec.epochyr + 2000;
+  } else {
+      year = satrec.epochyr + 1900;
+  }
+
+  var days2mdhmsResult = SGP4.days2mdhms(year, satrec.epochdays);
+  var mon, day, hr, minute, sec;
+  mon = days2mdhmsResult.mon;
+  day = days2mdhmsResult.day;
+  hr = days2mdhmsResult.hr;
+  minute = days2mdhmsResult.minute;
+  sec = days2mdhmsResult.sec;
+
+  satrec.jdsatepoch = SGP4.jday(year, mon, day, hr, minute, sec);
+
+  SGP4.sgp4init(gravconst, opsmode, satrec.satnum, satrec.jdsatepoch - 2433281.5, satrec.bstar, satrec.ecco, satrec.argpo, satrec.inclo, satrec.mo, satrec.no, satrec.nodeo, satrec);
+
+  return satrec;
 }
 
 function magnitude(vec) {
   return Math.sqrt(['x', 'y', 'z'].reduce((acc, k) => (acc + Math.pow(vec[k], 2)), 0));
 }
 
-function newSatRec(params = {}) {
-  const tle = params2tle(params).split('\n');
-  return satellite.twoline2satrec(tle[0], tle[1]);
+function invalidVector(vec) {
+  return !(vec && Number(vec.x) === vec.x && Number(vec.y) === vec.y && Number(vec.z) === vec.z)
 }
 
 const ERRORS = {
@@ -55,35 +99,60 @@ const ERRORS = {
 };
 
 function errorMessage(code) {
-  return ERRORS[code.toString()];
+  return ERRORS[code.toString()] || 'Error';
 }
 
-const TCanvas = ({ windowSize, position, velocity }) => {
+const TCanvas = ({ windowSize, position, velocity, semimajorAxis, eccentricity, inclination, rightAsc, argOfPerigee, error }) => {
   const vectorScale = windowSize / 100;
+  const semiminorAxis = Math.sqrt(Math.pow(semimajorAxis, 2) * (1 - Math.pow(eccentricity, 2)));
 
   return (
-    <svg viewBox={[windowSize / 2 * -1, windowSize / 2 * -1, windowSize, windowSize].join(' ')}>
-      <line x1={windowSize / 2 * -1} y1='0' x2={windowSize / 2} y2='0'
-        strokeWidth='10' stroke='black' strokeDasharray='500, 200' />
-      <line x1='0' y1={windowSize / 2 * -1} x2='0' y2={windowSize / 2}
-        strokeWidth='10' stroke='black' strokeDasharray='500, 200' />
-      <circle
-        cx={0}
-        cy={0}
-        r={EARTH_RADIUS}
-        fill='lightgrey'
-      />
-      <circle
-        cx={position.x}
-        cy={position.y}
-        r={200}
-        fill={'none'}
-        stroke='green'
-        strokeWidth={'50'}
-      />
-      <line x1={position.x} y1={position.y} x2={position.x + velocity.x * vectorScale} y2={position.y} strokeWidth='10' stroke='blue' />
-      <line x1={position.x} y1={position.y} x2={position.x} y2={position.y + velocity.y * vectorScale} strokeWidth='10' stroke='blue' />
-    </svg>
+    <div className='svgContainer'>
+      {error ? (
+        <div className='error'>
+          {error}
+        </div>
+      ) : (
+        <svg viewBox={[windowSize / 2 * -1, windowSize / 2 * -1, windowSize, windowSize].join(' ')}>
+          <line x1={windowSize / 2 * -1} y1='0' x2={windowSize / 2} y2='0'
+            strokeWidth='10' stroke='black' strokeDasharray='500, 200' />
+          <line x1='0' y1={windowSize / 2 * -1} x2='0' y2={windowSize / 2}
+            strokeWidth='10' stroke='black' strokeDasharray='500, 200' />
+          <circle
+            cx={0}
+            cy={0}
+            r={EARTH_RADIUS}
+            fill='lightgrey'
+          />
+          <circle
+            cx={position.x}
+            cy={position.y}
+            r={200}
+            fill={'none'}
+            stroke='green'
+            strokeWidth={'50'}
+          />
+          <ellipse
+            cx='0'
+            cy='0'
+            rx={semimajorAxis}
+            ry={semiminorAxis}
+            fill='none'
+            stroke='blue'
+            strokeWidth='20'
+            style={{
+              transform: [
+                `rotate3d(0,1,0,${inclination}deg)`,
+                `rotate3d(1,0,0,${rightAsc}deg)`,
+                `rotate3d(0,0,1,${argOfPerigee}deg)`,
+              ].join(' '),
+            }}
+          />
+          <line x1={position.x} y1={position.y} x2={position.x + velocity.x * vectorScale} y2={position.y} strokeWidth='10' stroke='blue' />
+          <line x1={position.x} y1={position.y} x2={position.x} y2={position.y + velocity.y * vectorScale} strokeWidth='10' stroke='blue' />
+        </svg>
+      )}
+    </div>
   );
 };
 
@@ -165,7 +234,14 @@ class TCanvasContainer extends Component {
     const dt = t - this.state.t0;
     const t0 = t;
     const date = this.state.d0.add(dt * this.state.rate, 's').toDate();
-    const rv = satellite.propagate(this.state.satrec, date);
+    var year = date.getUTCFullYear(),
+        month = date.getUTCMonth(),
+        day = date.getUTCDate(),
+        hours = date.getUTCHours(),
+        minutes = date.getUTCMinutes(),
+        seconds = date.getUTCSeconds();
+
+    var rv = SGP4.propogate(this.state.satrec, year, month + 1, day, hours, minutes, seconds);
     const error = this.state.satrec.error;
     if (error !== 0) console.log(this.state.satrec.error);
     this.setState({ t0, dt, date, rv, error });
@@ -186,28 +262,32 @@ class TCanvasContainer extends Component {
   }
 
   render() {
-    const { rv, date } = this.state;
+    const { rv, date, semimajorAxis, eccentricity, inclination, rightAsc, argOfPerigee } = this.state;
     const { position, velocity } = rv;
-    const error = this.state.error !== 0 && errorMessage(this.state.error);
+    const error = (this.state.error !== 0 || invalidVector(position) || invalidVector(velocity)) && errorMessage(this.state.error);
 
     return (
       <div className='TCanvasContainer'>
-        {this.state.error === 0 ? (
-          <div>
-            <TCanvas
-              windowSize={this.state.windowSize}
-              position={position}
-              velocity={velocity}
-            />
-            <TCanvasDetails
-              position={position}
-              velocity={velocity}
-            />
-          </div>
-        ) : (
+        <TCanvas
+          windowSize={this.state.windowSize}
+          position={position}
+          velocity={velocity}
+          semimajorAxis={semimajorAxis}
+          eccentricity={eccentricity}
+          inclination={inclination}
+          rightAsc={rightAsc}
+          argOfPerigee={argOfPerigee}
+          error={error}
+        />
+        {error ? (
           <div className='error'>
             {errorMessage(this.state.error)}
           </div>
+        ) : (
+          <TCanvasDetails
+            position={position}
+            velocity={velocity}
+          />
         )}
         <div className='controls'>
           <div className='row'>
